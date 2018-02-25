66ed11c1da2fc16ffdee2c45e8c5e786e5841b82
meenanehru <36810964+meenanehru@users.noreply.github.com>
2018-02-25 14:39:42 +0530

    Create e2.c

---
+++ e2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int n;
+   clrscr();
+   printf("n=");
+   scanf("%d",&n);
+   if(n%2==0)
+   {
+      printf("even");
+   }
+   else
+   {
+            printf("odd");
+    }
+    getch();
+   }
+      
