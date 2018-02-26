a0341c2de0ce50b4734f3e7f149d00ccedea7f7f
meenanehru <36810964+meenanehru@users.noreply.github.com>
2018-02-26 16:23:25 +0530

    Update e3.c

--- e3.c
+++ e3.c
@@ -7,11 +7,11 @@ int main(void)
 	scanf("%c",&n);
 	if(n=='a' && n=='e' && n=='i' && n=='o' && n=='u')
 	{
-	    printf("it is a vowel");
+	    printf(" vowel");
 	}
     else
     {
-    	printf("it is not a vowel");
+    	printf("constant");
     }
 	return 0;
 }
