4b09e3799c5faa05ab213cfd0bcf555d848907a4
meenanehru <36810964+meenanehru@users.noreply.github.com>
2018-02-27 13:47:42 +0530

    Create es20.c

---
+++ es20.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    
+	int n;
+	printf("Enter the value of n :");
+	scanf("%d",&n);
+	printf("\n%d",(1*n));
+	printf("\n%d",(2*n));
+	printf("\n%d",(3*n));
+	printf("\n%d",(4*n));
+	printf("\n%d",(5*n));
+
+	return 0;
+}
+
+
