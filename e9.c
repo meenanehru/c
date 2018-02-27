01df9d3f310250514a4691e56f68599561c2e209
meenanehru <36810964+meenanehru@users.noreply.github.com>
2018-02-27 12:28:39 +0530

    Create e9.c

---
+++ e9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    
+	int n[10],k,i,n1;
+	printf("Enter the number :");
+    scanf("%d",&n1);
+    for(i=0;i<n1;i++)
+    {
+        printf("n%d=",i);
+        scanf("%d",&n[i]);
+    }
+    printf("k=");
+    scanf("%d",&k);
+    printf("%d",n[k]);
+	return 0;
+}
+
+
+
