4e05a3a3661a330da82a2356cec3bdf23efb1508
meenanehru <36810964+meenanehru@users.noreply.github.com>
2018-02-27 13:24:06 +0530

    Create e6.c

---
+++ e6.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    
+	int year;
+	printf("Enter the year :");
+	scanf("%d",&year);
+	if(year%4==0)
+	{
+		if(year%100==0)
+		{
+			if(year%400==0)
+			{
+				printf("yes");
+			}
+			else 
+			{
+				printf("no");
+			}
+		}
+		else
+		{
+	
+		      printf("yes");
+	    }
+	}
+	else 
+	{
+		printf("no");
+	}
+	
+	return 0;
+}
+
+
+
