ff2134398c48ffb1e92db19be8dc6cd4301ea8eb
meenanehru <36810964+meenanehru@users.noreply.github.com>
2018-03-04 19:29:48 +0530

    Update e8.c

--- e8.c
+++ e8.c
@@ -3,10 +3,18 @@ int main()
 {
 	int a,t=0,i;
 	scanf("%d",&a);
-	for(i=1;i<=a;i++)
+	if(a>0)
 	{
-		t=i+t;
+		for(i=1;i<=a;i++)
+		{
+			t=i+t;
+		}
+		
+		printf("%d",t);
 	}
-	
-	printf("%d",t);
+	else
+	{
+		printf("enter natural numbers");
+	}
+
 }
